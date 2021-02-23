ff7d179930d4145253b6495cf67f072cc35059f3
Andreas Petersson <ap223ga@student.lnu.se>
2021-02-23 14:05:26 +0100

    More advances file selection

--- public/js/scan.js
+++ public/js/scan.js
@@ -4,19 +4,30 @@
   const electron = require('electron')
   // const { createReadStream } = require('fs')
   const path = require('path')
+  const fileService = require('fs')
   const dialog = electron.remote.dialog
+
+  let selectedFiles = []
+  let fileSelectMode = 'openFile'
+
   const selectFilesButton = document.getElementById('selectFilesButton')
 
   selectFilesButton.addEventListener('click', () => {
     if (process.platform !== 'darwin') {
       openFolderSelectDialog().then(file => {
-        postFile(file)
+        if (!file.canceled) {
+          selectedFiles = []
+          addFileToList(file)
+        }
       }).catch(err => {
         console.log(err)
       })
     } else {
       openFolderSelectDialog().then(file => {
-        postFile(file)
+        if (!file.canceled) {
+          selectedFiles = []
+          addFileToList(file)
+        }
       }).catch(err => {
         console.log(err)
       })
@@ -29,25 +40,27 @@
   singleFileModeRadioButton.addEventListener('change', (event) => {
     if (singleFileModeRadioButton.checked) {
       console.log('checked: singleFileModeRadioButton')
+      fileSelectMode = 'openFile'
     }
   })
 
   directoryModeRadioButton.addEventListener('change', (event) => {
     if (directoryModeRadioButton.checked) {
       console.log('checked: directoryModeRadioButton')
+      fileSelectMode = 'openDirectory'
     }
   })
 
-  function postFile (file) {
-    if (!file.canceled) {
-      global.filepath = file.filePaths[0].toString()
-      console.log(global.filepath)
-      /*
+  const startScanButton = document.getElementById('startScanButton')
+
+  startScanButton.addEventListener('click', (event) => {
+    console.log('START SCAN')
+    console.log(selectedFiles)
+    /*
       const stream = createReadStream(global.filepath)
       fetch('http://localhost:8080/', { method: 'POST', body: stream }) //eslint-disable-line
-      */
-    }
-  }
+     */
+  })
 
   function openFolderSelectDialog () {
     return dialog.showOpenDialog({
@@ -55,7 +68,7 @@
       defaultPath: path.join(__dirname, '../assets/'),
       buttonLabel: 'Select',
       filters: getFilters(),
-      properties: ['openFile']
+      properties: [fileSelectMode]
     })
   }
 
@@ -77,5 +90,23 @@
         extensions: javaFilesExt
       }
     ]
+  }
+
+  function findFilesRecursive (Directory) {
+    fileService.readdirSync(Directory).forEach(File => {
+      const absolutePath = path.join(Directory, File)
+      if (fileService.statSync(absolutePath).isDirectory()) return findFilesRecursive(absolutePath)
+      else return selectedFiles.push(absolutePath)
+    })
+  }
+
+  function addFileToList (file) {
+    console.log(file.filePaths[0].toString())
+    const filePath = file.filePaths[0].toString()
+    if (fileService.statSync(filePath).isDirectory()) {
+      findFilesRecursive(filePath)
+    } else {
+      selectedFiles.push(file.filePaths[0].toString())
+    }
   }
 })()
